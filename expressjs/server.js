0bb0e000ac32e2dcc18ce400dc3544cb70133633
HuiBeom <gmlqja7913@daum.net>
2025-09-30 02:29:52 +0900

    mongodb connect

--- expressjs/server.js
+++ expressjs/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const usersRouter = require('./routes/users_Router');
 const postsRouter = require('./routes/post_Router');
+const mongoose = require("mongoose");
 
 const PORT = 4000;
 
@@ -9,7 +10,9 @@ const PORT = 4000;
 
 const app = express();
 
-
+mongoose.connect('mongodb+srv://gmlqja7913_db_user:Efvj5pp6IJCEoBVI@cluster0.b7gweha.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
